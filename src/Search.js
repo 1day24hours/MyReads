4ef56ad909c5ee54ec44ef61a360e3dc98d4dc17
Felix Chen <chen.felix.xi@gmail.com>
2018-07-09 11:02:36 +0800

    v1.9 add 'autoFocus'

--- src/Search.js
+++ src/Search.js
@@ -41,6 +41,7 @@ class Search extends Component {
                     <Link className='close-search' to='/'>Close</Link>
                     <div className='search-books-input-wrapper'>
                         <input
+                            autoFocus
                             type='text'
                             placeholder='Search by title or author'
                             onChange={(e) => this.updateQuery(e.target.value)}
