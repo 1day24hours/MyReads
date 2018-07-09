673afd89a1a12b098c9b4ac990e0f73fd57ea03f
Felix Chen <chen.felix.xi@gmail.com>
2018-07-09 10:24:23 +0800

    v1.8 Book.js code optimized

--- src/Book.js
+++ src/Book.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 class Book extends Component {
 
-    static PropTypes = {
+    static propTypes = {
         book: PropTypes.object.isRequired,
         onMoveBook: PropTypes.func.isRequired
     }
@@ -14,15 +14,22 @@ class Book extends Component {
 
     render() {
         const { book } = this.props;
+        //把 style 抽离出来，方便维护
+        const BookCoverStyle = (book) => {
+            return {
+                width: 128,
+                height: 193,
+                //判断 book.imageLinks 是否存在，不存在便为其指定默认封面。
+                backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : "https://books.google.com/googlebooks/images/no_cover_thumb.gif"})`
+             }
+        }
 
         return(
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193,
-                    //判断 book.imageLinks 是否存在，不存在便为其指定默认封面。
-                        backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : "https://books.google.com/googlebooks/images/no_cover_thumb.gif"})` }}></div>
+                    <div className="book-cover" style={BookCoverStyle(book)}></div>
                     <div className="book-shelf-changer">
-                        <select value={book.shelf} onChange={(e) => this.updateBook(e.target.value)} >
+                        <select value={book.shelf ? book.shelf : "none"} onChange={(e) => this.updateBook(e.target.value)} >
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
